5cda16744afc85b6947db455ba9b47eb790abeba
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2022-04-09 15:06:08 +0900

    create another package.json

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const colors = require("colors");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv").config({ path: "../.env" });
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
@@ -14,5 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api", require("./routs/otterRouts"));
 
 app.use("/collection", require("./routs/collectionRouts"));
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.listen(port, () => console.log(`server started on port ${port}`));
